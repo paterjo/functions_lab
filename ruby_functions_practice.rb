0876626a093f9518db3ea60d1c530582b69f7ead
moira <johnandmoira@live.co.u>
2020-02-19 09:35:05 +0000

    first commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,23 @@
+def return_10()
+  return 10
+end
+
+def add(num1, num2)
+  return num1.to_i + num2.to_i
+end
+
+def subtract(num1, num2)
+  return num1.to_i - num2.to_i
+end
+
+def multiply(num1, num2)
+  return num1.to_i * num2.to_i
+end
+
+def divide(num1, num2)
+  return num1.to_i / num2.to_i
+end
+
+def length_of_string(str)
+  return length_of_string()
+end
